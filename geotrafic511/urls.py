535de31ddc602459b9a236618b564bb443eeb8db
Michael Mulley <michael@michaelmulley.com>
2015-11-26 14:35:53 -0500

    Rearrange URL structure

--- geotrafic511/urls.py
+++ geotrafic511/urls.py
@@ -5,15 +5,19 @@ from django.contrib import admin
 from .views import simple_index_page
 
 urlpatterns = [
-    url(r'^api/', include('open511_server.urls')),
-
     url(r'^carte/', include('django_open511_ui.urls')),
-    url(r'^accounts/', include('django_open511_ui.auth_urls')),
+    
+    url(r'', include('open511_server.urls')),
 
-    url(r'^admin/', admin.site.urls),
-
-    url(r'^$', simple_index_page),
+    #url(r'^$', simple_index_page),
 ]
+
+if getattr(settings, 'ENABLE_ADMIN', None):
+    admin.site.site_header = 'Administration Open511'
+    urlpatterns += [
+        url(r'^accounts/', include('django_open511_ui.auth_urls')),
+        url(r'^admin/', admin.site.urls),
+    ]
 
 if getattr(settings, 'URL_PREFIX', None):
     urlpatterns = [url('^' + settings.URL_PREFIX, include(urlpatterns))]
