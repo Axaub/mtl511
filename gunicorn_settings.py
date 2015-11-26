5487a515ed29671122ec2e46e913f8ec6532a30d
Michael Mulley <michael@michaelmulley.com>
2015-11-26 11:59:05 -0500

    gunicorn logging

--- gunicorn_settings.py
+++ gunicorn_settings.py
@@ -1,6 +1,15 @@
 from multiprocessing import cpu_count
-from os import environ
+import os
 
-bind = '0.0.0.0:' + environ.get('OPEN511_PORT', '42000')
+bind = '0.0.0.0:' + os.environ.get('OPEN511_PORT', '42000')
 workers = cpu_count() + 1
 worker_class = 'eventlet'
+
+proc_name = 'open511-web-server'
+
+_log_dir = os.path.join(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+    'logs')
+
+accesslog = os.path.join(_log_dir, 'web_access')
+errorlog = os.path.join(_log_dir, 'web_error')
